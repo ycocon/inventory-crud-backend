7ec9fd41ea172c0fbb7535ee1909e0d683d570bd
Andrei De Jesus <andreidejesus06@gmail.com>
2023-08-29 18:10:28 +0800

    added cors middleware

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const productRoutes = require("./routes/product");
 
 const app = express();
 
 // middleware
+app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
